0b1a74ec2e75b66cdf1b5b8399681ea504c480a6
Munishwar kalra <kalramunishwar.com>
2026-03-12 18:15:48 +0530

    added the integration of material ui and tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./core/components/landing-layout/landing-layout').then((m) => m.LandingLayout),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./core/components/dashboard-layout/dashboard-layout').then((m) => m.DashboardLayout),
+  },
+];
---
+++ src/app/core/components/landing-layout/landing-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-landing-layout',
+  imports: [MatButtonModule],
+  templateUrl: './landing-layout.html',
+  styleUrl: './landing-layout.scss',
+})
+export class LandingLayout {
+
+}
