cfd4654611749e7aafc1b9c3052b4b45f0279f18
Munishwar kalra <kalramunishwar.com>
2026-03-19 18:09:02 +0530

    add auth module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,14 @@ export const routes: Routes = [
       import('./core/components/landing-layout/landing-layout').then((m) => m.LandingLayout),
   },
   {
+    path: 'login',
+    loadComponent: () => import('./auth/components/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./auth/components/signup/signup').then((m) => m.Signup),
+  },
+  {
     path: 'dashboard',
     loadComponent: () =>
       import('./core/components/dashboard-layout/dashboard-layout').then((m) => m.DashboardLayout),
